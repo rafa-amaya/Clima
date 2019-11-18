61a7b5a3bad96262a46d2447fcffc7283df00b95
Rafael Amaya <rafa.amaya82@gmail.com>
2019-11-18 14:58:42 -0300

    Se agrego el NuGet MvvmLigthLibsStd10 y la interface INotifyPropertyChanged

--- Clima.Forms/Clima.Forms/ViewModels/HomeViewModel.cs
+++ Clima.Forms/Clima.Forms/ViewModels/HomeViewModel.cs
@@ -1,8 +1,26 @@
 ﻿
 
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+
 namespace Clima.Forms.ViewModels
 {
-    public class HomeViewModel
+    public class HomeViewModel : BaseViewModel
     {
+        public string title;
+        public string Title
+        {
+            get { return this.title; }
+            set { this.SetValue(ref this.title, value); }
+        }
+
+
+        public ICommand GetWatherCommand => new RelayCommand(GetWather);
+
+        private void GetWather()
+        {
+            this.Title = "Buenos Aires";
+        }
     }
 }
