96eadf87ed5f613e4c76a96bbc7f18b62e3295fe
Rafael Amaya <rafa.amaya82@gmail.com>
2019-11-19 02:44:30 -0300

    Integrado el Splash

---
+++ Clima.Forms/Clima.Forms.Android/SplashActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using Clima.Forms.Droid;
+
+[Activity(
+    Theme = "@style/Theme.Splash",
+    MainLauncher = true,
+    NoHistory = true)]
+public class SplashActivity : Activity
+{
+    protected override void OnCreate(Bundle bundle)
+    {
+        base.OnCreate(bundle);
+        System.Threading.Thread.Sleep(1800);
+        this.StartActivity(typeof(MainActivity));
+    }
+}
